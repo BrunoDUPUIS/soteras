732fa76cb5293e86ddafb8840ea932c63ff2182f
Bruno <b-d.dupuis@laposte.net>
2026-05-19 20:13:48 +0200

    fourth commit

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -15,18 +15,18 @@ import CTA from '../components/CTA.vue'
         <ServiceCard
           title="Plomberie"
           description="Installation et dépannage plomberie"
-          image="public/images/ecs.jpeg"
+          image="public/images/ecs.webp"
         />
 
         <ServiceCard
           title="Chauffage"
           description="Installation de chauffage moderne"
-          image="public/images/thermo.jpeg"
+          image="public/images/thermo.webp"
         />
         <ServiceCard
           title="Dépannage"
           description="Intervention rapide 7j/7"
-          image="public/images/depannage.jpeg"
+          image="public/images/depannage.webp"
         />
       </div>
     </section>
--- src/pages/Services.vue
+++ src/pages/Services.vue
@@ -10,19 +10,19 @@ import ServiceCard from '../components/ServiceCard.vue'
       <ServiceCard
         title="Salle de bain"
         description="Création et rénovation complète"
-        image="public/images/sbd.jpeg"
+        image="public/images/sbd.webp"
       />
 
       <ServiceCard
         title="Recherche de fuite"
         description="Détection et réparation rapide"
-        image="public/images/recherche-de-fuite.jpeg"
+        image="public/images/recherche-de-fuite.webp"
       />
 
       <ServiceCard
         title="Pompe à chaleur"
         description="Solutions économiques et performantes"
-        image="public/images/pac.jpeg"
+        image="public/images/pac.webp"
       />
     </div>
   </section>
