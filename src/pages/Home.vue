8524aba6da8869433845fecec7ccb99a157f470f
Bruno <b-d.dupuis@laposte.net>
2026-05-19 20:37:42 +0200

    seventh commit

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -15,18 +15,18 @@ import CTA from '../components/CTA.vue'
         <ServiceCard
           title="Plomberie"
           description="Installation et dépannage plomberie"
-          image="/images/ecs.webp"
+          image="./images/ecs.webp"
         />
 
         <ServiceCard
           title="Chauffage"
           description="Installation de chauffage moderne"
-          image="/images/thermo.webp"
+          image="./images/thermo.webp"
         />
         <ServiceCard
           title="Dépannage"
           description="Intervention rapide 7j/7"
-          image="images/depannage.webp"
+          image="./images/depannage.webp"
         />
       </div>
     </section>
