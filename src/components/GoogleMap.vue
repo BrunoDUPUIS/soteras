3ea30ec1752196f495d11205ea658de56523b99a
Bruno <b-d.dupuis@laposte.net>
2026-05-20 21:57:13 +0200

    test modif

--- src/components/GoogleMap.vue
+++ src/components/GoogleMap.vue
@@ -3,7 +3,7 @@
     <iframe
       src="https://www.google.com/maps?q=220+Rue+Jean+Rostand,+40465+Pontonx-sur-l%27Adour&output=embed"
       width="100%"
-      height="450"
+      height="300"
       style="border: 0"
       allowfullscreen=""
       loading="lazy"
